9cbb37123e2852b95ee6abe12a50431a51457713
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-10-18 05:31:24 -0400

    removed print statement

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ $(function (){
     let addition = $("li:first-child").html() 
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault()
-        console.log("Is it working")
         let newShoppingItem = $(':input').val()
         $("ul").append("<li>" + addition + "</li>")
         $("li:last-child").find(".shopping-item").text(newShoppingItem)
     })
 
     $("ul").on('click', '.shopping-item-toggle', function(e){
-        console.log("bruh")
         e.stopPropagation();
         let clickedEl = $(this).parents("li").find(".shopping-item")
         clickedEl.toggleClass("shopping-item__checked")
