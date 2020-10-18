7378966adbd0fdc58a8339e171273157278ff15a
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-10-18 05:27:04 -0400

    meets specified requirements

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-function addItem(){
-    $('js-shopping-list-form').submit(e => {
-        let newShoppingItem = $('shopping-list-entry').text()
-        $("ul").append($("li").attr("shopping-item").text(newShoppingItem))
-    }
-        
-        
-        )
-}
 
-$(addItem())
+
+$(function (){  
+    let addition = $("li:first-child").html() 
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault()
+        console.log("Is it working")
+        let newShoppingItem = $(':input').val()
+        $("ul").append("<li>" + addition + "</li>")
+        $("li:last-child").find(".shopping-item").text(newShoppingItem)
+    })
+
+    $("ul").on('click', '.shopping-item-toggle', function(e){
+        console.log("bruh")
+        e.stopPropagation();
+        let clickedEl = $(this).parents("li").find(".shopping-item")
+        clickedEl.toggleClass("shopping-item__checked")
+      })
+      $("ul").on('click', ".shopping-item-delete", function(e){
+          e.stopPropagation()
+          $(this).closest("li").remove()
+      }) 
+    
+})
+
